13724c4cdb89586f5f864450940c62e170ec880c
mantos <matt.antos@gmail.com>
2022-12-12 12:00:12 -0500

    Stashing stable "table"

--- pages/Components/SearchDisplayComponents/PostResultPanel.tsx
+++ pages/Components/SearchDisplayComponents/PostResultPanel.tsx
@@ -3,64 +3,63 @@ import React, { useState, useEffect, Key } from 'react';
 
 import { IPostData } from 'data-service/types';
 
-import { Table, Column, Index, TableHeaderRowProps, TableRowProps } from 'react-virtualized';
-import 'react-virtualized/styles.css';
+import { useVirtualizer } from '@tanstack/react-virtual';
 
 import { useRecoilValueLoadable, useRecoilValue, useRecoilState, useRecoilStateLoadable } from 'recoil';
 import {  searchRequestState } from '../contexts/SearchContext';
 
-
+import styles from '../../../styles/Components/Form.module.scss';
+import { truncEllipsis } from '../FormUtils';
 
 
 const TitleColumn = {
     key: 'title',
     name: 'Post Title',
-    // headerRenderer: (p) => (
-    //     <FilterRenderer<IPostData, unknown, HTMLInputElement> {...p}>
-    //         {({ filters, ...rest }) => (
-    //             <input
-    //                 {...rest}
-    //             />
-    //         )}
-    //     </FilterRenderer>
-    // ),
-    sortable: true
+    className: 'is-3',
+    dataRenderer : (value:any) => {
+        return value;
+    }
 };
 const OrgColumn = {
     key: 'organization',
     name: 'Organization',
-    sortable: true
+    className: 'is-2'
 };
 const LocColumn = {
     key: 'location',
     name: 'Location',
-    sortable: true
+    className: 'is-narrow'
 };
 const DescColumn = {
     key: 'description',
     name: 'Post Desc.',
-    sortable: true
+    className: 'is-3',
+    dataRenderer : (value:any) => {
+        return truncEllipsis(value, 100);
+    }
 };
 const CapTimeColumn = {
     key: 'captureTime',
     name: 'Updated Time',
-    sortable: true
+    className: 'is-1',
+    dataRenderer : (value:any) => {
+        const date = new Date(value);
+        return date.toLocaleString();
+    }
 };
 const SalaryColumn = {
     key: 'salary',
     name: 'Salary Info',
-    sortable: true
+    className: 'is-1'
 };
 
 const IDColumn = {
     key: '_id',
     name: 'ID',
-    sortable: true
 };
 const UsrModColumn = {
     key: 'userModified',
     name: 'Edited',
-    sortable: true
 };
 const PostTimeColumn = {
     key: 'postedTime',
@@ -70,7 +69,6 @@ const PostTimeColumn = {
 const NoDataColumn = {
     key: 'msg',
     name: 'No Data / Info Table',
-    sortable: true
 };
 
 export const columns = [
@@ -85,48 +83,90 @@ export const columns = [
 
 const PostResultPanel = (props:any & {dataset?:IPostData[]} ) => {
     const requestAtom = useRecoilValue(searchRequestState);
+    const parentRef = React.useRef<HTMLDivElement>();
 
+    // The virtualizer
+    const rowVirtualizer = useVirtualizer({
+        count: (requestAtom.dataset === undefined)? 1: requestAtom.dataset.length,
+        getScrollElement: () => (parentRef.current === undefined)? null : parentRef.current,
+        estimateSize: () => 35,
+    });
+
+    if(requestAtom.dataset === undefined){
+        return (
+            <div>
+                <div className={['columns is-centered'].join(" ")}>
+                    <div className={['column'].join(" ")}>no data available</div>
+                </div>
+            </div>
+        );
+    }else{
+        const viewData:IPostData[] = requestAtom.dataset; //enforce datatype
+        return (
+            <div>
+                <table className='table is-bordered is-striped is-narrow is-hoverable is-fullwidth'>
+                    <thead>
+                        <tr>
+                            {columns.map((col)=>(
+                                <td>{col.name}</td>
+                            ))}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td colSpan={6}> nodata </td>
+                        </tr>
+                        <tr>
+                            <td colSpan={6}> nodata </td>
+                        </tr>
+                    </tbody>
+                </table>
+                <div
+                    ref={parentRef}
+                    style={{
+                        height: `400px`,
+                        overflow: 'auto', // Make it scroll!
+                    }}
+                >
+                    <div
+                        style={{
+                            height: `${rowVirtualizer.getTotalSize()}px`,
+                            width: '100%',
+                            position: 'relative',
+                        }}
+                    >
+                        
+                        {rowVirtualizer.getVirtualItems().map((virtualItem) => {
+                            const row = viewData[virtualItem.index] as any;
+                            return (
+                                <div className={['columns'].join(" ")}
+                                    key={virtualItem.key}
+                                    style={{
+                                        position: 'absolute',
+                                        top: 0,
+                                        left: 0,
+                                        width: '100%',
+                                        height: `${virtualItem.size}px`,
+                                        transform: `translateY(${virtualItem.start}px)`,
+                                    }}>
+
+                                    {columns.map((col)=>(
+                                        <div className={['column', col.className].join(" ")}>{(col.dataRenderer != undefined)? col.dataRenderer(row[col.key]) :row[col.key]}</div>
+                                    ))}
+                                    
+                                </div>
+                                    
+                            );
+                        })}
+                        
+                    </div>
+                </div>
+            </div>
+        );
+    }
     
-    return (
-        <Table headerHeight={55} height={800} rowHeight={55} width={1000} 
-            rowCount={(requestAtom.dataset)?requestAtom.dataset.length : 0}
-            headerRowRenderer={(props: TableHeaderRowProps) => {
-                return (
-                    <div className={['columns'].join(" ")}>
-                        {props.columns.map( (val,index) => {
-                            return (
-                                <div className={['column'].join(" ")}>
-                                    {val}
-                                </div>
-                            );
-                        } )}
-                    </div>
-                );
-            }}
-            rowRenderer={(props: TableRowProps) => {
-                return (
-                    <div className={['columns'].join(" ")}>
-                        {props.columns.map( (val,index) => {
-                            return (
-                                <div className={['column'].join(" ")}>
-                                    {val}
-                                </div>
-                            );
-                        } )}
-                    </div>
-                );
-            }}
-            rowGetter={(info:Index)=>{
-                return (requestAtom.dataset)? requestAtom.dataset[info.index] : undefined;
-            }}>
-            <Column dataKey={TitleColumn.key} label={TitleColumn.name} width={200}/>
-            <Column dataKey={OrgColumn.key} label={OrgColumn.name} width={100}/>
-            <Column dataKey={LocColumn.key} label={LocColumn.name} width={100}/>
-            <Column dataKey={DescColumn.key} label={DescColumn.name} width={100}/>
-            <Column dataKey={CapTimeColumn.key} label={CapTimeColumn.name} width={100}/>
-            <Column dataKey={SalaryColumn.key} label={SalaryColumn.name} width={100}/>
-        </Table>
-    );
+    
+    
    
 };
 
